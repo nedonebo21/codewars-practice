b004c271119920cce08f61160c73a9eb237243ee
nedonebo21 <cursed.void10007@gmail.com>
2025-06-12 15:59:21 +0400

    (7kyu kata) 12.06.2025

--- kata/12-06-2025.js
+++ kata/12-06-2025.js
@@ -12,3 +12,18 @@ const divCon = (x) => {
     )
     return numsSum - strSum
 }
+
+//https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/javascript
+//7kyu
+function arithmetic(a, b, operator){
+    switch(operator){
+        case 'add':
+            return a + b
+        case 'subtract':
+            return a - b
+        case 'multiply':
+            return a * b
+        case 'divide':
+            return a / b
+    }
+}
