2b4c9ec64958468d74221bb9cee9bcfc89a1b858
nedonebo21 <cursed.void10007@gmail.com>
2025-06-20 14:13:00 +0400

    (7kyu kata) 20.06.2025

--- kata/20-06-2025.js
+++ kata/20-06-2025.js
@@ -1,3 +1,7 @@
 //https://www.codewars.com/kata/55b051fac50a3292a9000025/train/javascript
 // 7kyu
 const filterString = (str) => parseInt(str.replace(/[a-z]/gi,''))
+
+//https://www.codewars.com/kata/5783d8f3202c0e486c001d23/train/javascript
+// 7kyu
+const toNumberArray = (arr) => arr.reduce((acc,curr) => [...acc, +curr], [])
