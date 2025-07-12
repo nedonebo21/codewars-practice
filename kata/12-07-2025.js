de4d6173e3c741fbac3629159f91d83b93aeda90
nedonebo21 <cursed.void10007@gmail.com>
2025-07-12 17:28:39 +0400

    (7kyu kata) 12.07.2025

--- kata/12-07-2025.js
+++ kata/12-07-2025.js
@@ -3,3 +3,10 @@
 const reverse = (array) => {
   return array.reduce((acc, curr) => [curr, ...acc], [])
 }
+
+//https://www.codewars.com/kata/56269eb78ad2e4ced1000013/train/javascript
+//7kyu
+const findNextSquare = (sq) => {
+  const perf = Math.sqrt(sq)
+  return perf % 1 === 0 ? (perf + 1) * (perf + 1) : -1
+}
