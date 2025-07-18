275869c813d813b0296cbaba227e627f30974135
nedonebo21 <cursed.void10007@gmail.com>
2025-07-18 16:16:18 +0400

    (7kyu kata) 18.07.2025

--- kata/18-07-2025.js
+++ kata/18-07-2025.js
@@ -3,3 +3,7 @@
 const pillars = (numPill,dist,width) => (
     numPill === 1 ? 0 : (numPill - 2) * width + (numPill - 1) * dist * 100
 )
+
+// https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/javascript
+// 7kyu
+const noOdds = (values) => values.filter(v => v % 2 === 0)
