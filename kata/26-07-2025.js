2fb97d31b46b70217ba820db96b1f66ded83fe16
nedonebo21 <cursed.void10007@gmail.com>
2025-07-26 11:41:26 +0400

    (7kyu kata) 26.07.2025

--- kata/26-07-2025.js
+++ kata/26-07-2025.js
@@ -1,6 +1,6 @@
 //https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/javascript
 //6kyu
-const deleteNth = (arr,n) => {
+const deleteNth = (arr, n) => {
   let elCounts = {}
   return arr.filter(el => (elCounts[el] = (elCounts[el] || 0) + 1) <= n)
 }
@@ -12,7 +12,24 @@ class Person {
     this.name = name,
         this.age = age
   }
-  get info(){
+
+  get info() {
     return `${this.name}s age is ${this.age}`
   }
 }
+
+//https://www.codewars.com/kata/57f759bb664021a30300007d/train/javascript
+//7kyu
+const switcheroo = (x) => (
+    x.split('')
+        .map(el => {
+          switch (el) {
+            case 'a':
+              return 'b';
+            case 'b':
+              return 'a';
+            default:
+              return el
+          }
+        }).join('')
+)
