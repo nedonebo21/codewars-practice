ff535fb0804ed9c3c5f4ed494cf653ed894bd238
nedonebo21 <cursed.void10007@gmail.com>
2025-07-26 11:26:39 +0400

    (8kyu kata) 26.07.2025

--- kata/26-07-2025.js
+++ kata/26-07-2025.js
@@ -4,3 +4,15 @@ const deleteNth = (arr,n) => {
   let elCounts = {}
   return arr.filter(el => (elCounts[el] = (elCounts[el] || 0) + 1) <= n)
 }
+
+//https://www.codewars.com/kata/55a144eff5124e546400005a/train/javascript
+//8kyu
+class Person {
+  constructor(name, age) {
+    this.name = name,
+        this.age = age
+  }
+  get info(){
+    return `${this.name}s age is ${this.age}`
+  }
+}
