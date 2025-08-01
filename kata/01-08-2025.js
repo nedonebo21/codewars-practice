4e9abb516606f88488485b3e4549d4a8f1e09fc6
nedonebo21 <cursed.void10007@gmail.com>
2025-08-01 17:30:06 +0400

    (7kyu kata) 01.08.2025

---
+++ kata/01-08-2025.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/5663f5305102699bad000056/train/javascript
+// 7kyu
+function mxdiflg(a1, a2) {
+
+  if (!a1.length || !a2.length) return -1
+
+  const length1 = a1.map(el => el.length)
+  const length2 = a2.map(el => el.length)
+
+  const Difference1 = Math.max(...length1) - Math.min(...length2)
+  const Difference2 = Math.max(...length2) - Math.min(...length1)
+
+  return Math.max(Difference1, Difference2)
+}
+
