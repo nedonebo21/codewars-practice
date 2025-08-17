4de855225e85ef4dbe9081a5bed328623e5526f9
nedonebo21 <cursed.void10007@gmail.com>
2025-08-17 15:08:37 +0400

    (8kyu kata) 17.08.2025

--- kata/17.08.2025.js
+++ kata/17.08.2025.js
@@ -1,3 +1,14 @@
 //https://www.codewars.com/kata/562926c855ca9fdc4800005b/train/javascript
 // 8kyu
 const numberToPower = (num, pow) => Array(pow).fill(num).reduce((acc, curr) => acc * curr, 1)
+
+//https://www.codewars.com/kata/56a946cd7bd95ccab2000055/train/javascript
+// 8kyu
+const lowercaseCount = (str) => (
+    str.split('').reduce((acc,char) => {
+      if (char !== char.toUpperCase() && char === char.toLowerCase()){
+        return acc + 1
+      }
+      return acc
+    }, 0)
+)
